26b5ab37066e4e97231663d92fc64d7d9e025063
clxhope <changluxin@gmail.com>
2017-04-06 10:26:17 +0800

    修改join和quit的提示信息

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -48,9 +48,9 @@ end
 
     if !current_user.is_member_of?(@group)
       current_user.join!(@group)
-      flash[:notice] = "加入本讨论版成功！"
+      flash[:notice] = "收藏电影成功！"
     else
-      flash[:warning] = "你已经是本讨论版成员了！"
+      flash[:warning] = "你已经收藏了这部电影！"
     end
 
     redirect_to group_path(@group)
@@ -61,9 +61,9 @@ end
 
     if current_user.is_member_of?(@group)
       current_user.quit!(@group)
-      flash[:alert] = "已退出本讨论版！"
+      flash[:alert] = "已取消收藏这部电影！"
     else
-      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+      flash[:warning] = "你没有收藏这部电影，怎么取消 XD"
     end
 
     redirect_to group_path(@group)
